0cd102d4318cf3278fb3ca27fb5a7417deafd1c6
idKrazu <idkrazu@gmail.com>
2021-04-06 01:24:09 +0200

    🐛 Fix multiple inputing multiple zeros at the start ✨ Add float numbers with second input number ✨ Add handling operators ✨ Test 16/16

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default class App extends Component {
       secondInput: "",
     };
     this.handleClick = this.handleClick.bind(this);
+    this.calculateResult = this.calculateResult.bind(this);
   }
 
   handleClick(e) {
@@ -26,52 +27,136 @@ export default class App extends Component {
     } else if (!isNaN(parseInt(key))) {
       // Numbers
       if (this.state.operators === "") {
-        this.setState((state) => ({
-          firstInput: state.firstInput + key,
-        }));
+        // Prevent from inputing zeros at the start
+        if (this.state.firstInput === "0" && key === "0") {
+          this.setState({
+            firstInput: "0",
+          });
+        } else if (this.state.firstInput === "0") {
+          this.setState({
+            firstInput: key,
+          });
+        } else {
+          this.setState((state) => ({
+            firstInput: state.firstInput + key,
+          }));
+        }
       } else {
-        this.setState((state) => ({
-          secondInput: state.secondInput + key,
-        }));
+        // Prevent from inputing zeros at the start
+        if (this.state.secondInput === "0" && key === "0") {
+          this.setState({
+            secondInput: "0",
+          });
+        } else if (this.state.secondInput === "0") {
+          this.setState({
+            secondInput: key,
+          });
+        } else {
+          this.setState((state) => ({
+            secondInput: state.secondInput + key,
+          }));
+        }
       }
     } else if (key === ".") {
       // Float point
-      if (!this.state.firstInput.includes(".")) {
-        this.setState((state) => ({
-          firstInput: state.firstInput + key,
-        }));
+      if (this.state.operators === "") {
+        if (!this.state.firstInput.includes(".")) {
+          this.setState((state) => ({
+            firstInput: state.firstInput + key,
+          }));
+        }
+      } else {
+        if (!this.state.secondInput.includes(".")) {
+          this.setState((state) => ({
+            secondInput: state.secondInput + key,
+          }));
+        }
       }
     } else if (key === "=") {
       // Result
       if (this.state.firstInput !== "" && this.state.secondInput !== "") {
-        let result = 0;
-        let firstNumber = parseInt(this.state.firstInput);
-        let secondNumber = parseInt(this.state.secondInput);
-        switch (this.state.operators) {
-          case "+":
-            result = firstNumber + secondNumber;
-            break;
-          case "-":
-            result = firstNumber - secondNumber;
-            break;
-          case "*":
-            result = firstNumber * secondNumber;
-            break;
-          case "/":
-            result = firstNumber / secondNumber;
-            break;
-        }
+        let result = this.calculateResult(
+          this.state.firstInput,
+          this.state.secondInput,
+          this.state.operators
+        );
         this.setState({
           firstInput: result,
           operators: "",
           secondInput: "",
         });
       }
-    } else {
-      this.setState({
-        operators: key,
-      });
+    } else if (["+", "-", "*", "/"].indexOf(key) !== -1) {
+      // Operators
+      // First number can be negative
+
+      if (
+        this.state.firstInput !== "" &&
+        this.state.operators !== "" &&
+        this.state.secondInput !== "" &&
+        this.state.secondInput !== "-"
+      ) {
+        let result = this.calculateResult(
+          this.state.firstInput,
+          this.state.secondInput,
+          this.state.operators
+        );
+        this.setState({
+          firstInput: result,
+          operators: key,
+          secondInput: "",
+        });
+      } else if (
+        this.state.firstInput !== "" &&
+        this.state.firstInput !== "-"
+      ) {
+        if (this.state.operators === "") {
+          this.setState({
+            operators: key,
+          });
+        } else if (key === "-") {
+          this.setState({
+            secondInput: key,
+          });
+        } else if (this.state.operators !== "") {
+          this.setState({
+            operators: key,
+            secondInput: "",
+          });
+        }
+      } else if (this.state.firstInput === "" && key === "-") {
+        this.setState({
+          firstInput: key,
+        });
+      } else if (this.state.firstInput === "-" && key === "+") {
+        this.setState({
+          firstInput: "",
+        });
+      }
     }
+  }
+
+  calculateResult(firstNumber, secondNumber, operator) {
+    let result = 0;
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    switch (operator) {
+      case "+":
+        result = firstNumber + secondNumber;
+        break;
+      case "-":
+        result = firstNumber - secondNumber;
+        break;
+      case "*":
+        result = firstNumber * secondNumber;
+        break;
+      case "/":
+        result = firstNumber / secondNumber;
+        break;
+      default:
+        break;
+    }
+    return result.toString();
   }
 
   render() {
